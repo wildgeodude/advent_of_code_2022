a6cf708d6b96edccdddd3d723d2f656afa7b4f74
wildgeodude <dominic.samphier@gmail.com>
2022-12-16 07:25:58 +0000

    name changes

--- day_1/script.rb
+++ day_1/script.rb
@@ -7,15 +7,15 @@ def inventory_greater_than_zero?(inventory)
 end
 
 # read lines and process the txt file
-array_of_inventories = File.readlines('inventory.txt', "\n\n").map{ |s| s.rstrip.split("\n") }
+parsed_and_formatted_inventories = File.readlines('inventory.txt', "\n\n").map{ |s| s.rstrip.split("\n") }
 
 # convert all values in arrays into numbers
-array_of_inventories = array_of_inventories.map { |inventory| inventory.map(&:to_i) }
+array_of_inventories = parsed_and_formatted_inventories.map { |inventory| inventory.map(&:to_i) }
 
 # ensure all inventories total one or more
 array_of_inventories.all? { |inventory| inventory_greater_than_zero?(inventory) }
 
 # sum each array
-array_of_inventories = array_of_inventories.map { |inventory| inventory.sum }
+array_of_inventories_by_size = array_of_inventories.map { |inventory| inventory.sum }
 
-puts "Elf #{array_of_inventories.each_with_index.max[1]} is a chonker, carrying #{array_of_inventories.each_with_index.max[0]} calories"
+puts "Elf #{array_of_inventories_by_size.each_with_index.max[1]} is a chonker, carrying #{array_of_inventories_by_size.each_with_index.max[0]} calories"
